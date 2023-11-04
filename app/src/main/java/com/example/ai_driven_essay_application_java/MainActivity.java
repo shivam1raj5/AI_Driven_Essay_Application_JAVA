e6fa021d6385ed94669527adf4bc4bcc43f3a4dc
Shivam Raj <shivam1raj5@gmail.com>
2023-11-05 03:16:47 +0530

    Done Working Fine

--- app/src/main/java/com/example/ai_driven_essay_application_java/MainActivity.java
+++ app/src/main/java/com/example/ai_driven_essay_application_java/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ai_driven_essay_application_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ques = findViewById(R.id.ques);
-        tag = findViewById(R.id.tag);
+        ques = (EditText) findViewById(R.id.ques);
+        tag = (EditText) findViewById(R.id.tag);
         lang = (Spinner) findViewById(R.id.lang);
-        size = findViewById(R.id.size);
+        size = (EditText) findViewById(R.id.size);
         btn = findViewById(R.id.btn);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -33,12 +36,22 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         lang.setAdapter(adapter);
 
-        String question = ques.getText().toString();
-        String tags = tag.getText().toString();
-        String language = lang.getSelectedItem().toString();
-        String wordsize = size.getText().toString();
 
-        String q = question+tags+language+wordsize+"words";
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = ques.getText().toString();
+                String tags = tag.getText().toString();
+                String language = lang.getSelectedItem().toString();
+                String wordsize = size.getText().toString();
+
+                String final_question = question+" "+tags+" "+language+" "+wordsize+" "+"words";
+                Intent intent = new Intent(MainActivity.this, output.class);
+                intent.putExtra("question",final_question);
+                startActivity(intent);
+            }
+        });
 
 
 
