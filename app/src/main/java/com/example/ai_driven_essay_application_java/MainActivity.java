17e5c544dba09c9ffb691a73b3e7c10f9283c0bc
Shivam Raj <shivam1raj5@gmail.com>
2023-11-05 00:49:59 +0530

    Question page backend added and Result page created

--- app/src/main/java/com/example/ai_driven_essay_application_java/MainActivity.java
+++ app/src/main/java/com/example/ai_driven_essay_application_java/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.ai_driven_essay_application_java;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ques;
+    EditText tag;
+    Spinner lang;
+    EditText size;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ques = findViewById(R.id.ques);
+        tag = findViewById(R.id.tag);
+        lang = (Spinner) findViewById(R.id.lang);
+        size = findViewById(R.id.size);
+        btn = findViewById(R.id.btn);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.language_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lang.setAdapter(adapter);
+
+        String question = ques.getText().toString();
+        String tags = tag.getText().toString();
+        String language = lang.getSelectedItem().toString();
+        String wordsize = size.getText().toString();
+
+        String q = question+tags+language+wordsize+"words";
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/ai_driven_essay_application_java/output.java
@@ -0,0 +1,14 @@
+package com.example.ai_driven_essay_application_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class output extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output);
+    }
+}
